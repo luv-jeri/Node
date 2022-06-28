fbef5d7cd01894b58b9f36575eefcaf0f0832fb6
luv-jeri <luvvjeri@gmail.com>
2022-06-28 21:56:51 +0530

    -

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const http = require('http');
 const moment = require('moment');
 
-const serverFunction = (req, res) => {
-  res.end('Hello World !');
-};
+const server = http.createServer((req, res) => {
+  const { url } = req;
+  const data = [
+    { name: 'John', age: 20 },
+    { name: 'Jane', age: 21 },
+    { name: 'Joe', age: 22 },
+  ];
+  res.writeHead(200, { 'Content-Type': 'application/json' });
 
-const server = http.createServer(serverFunction);
+  if (url === '/') {
+    res.end(
+      JSON.stringify({
+        message: 'Hello World',
+      })
+    );
+  }
+
+  if (url === '/time') {
+    res.end(
+      JSON.stringify({
+        time: moment().format('h:mm:ss a'),
+      })
+    );
+  }
+
+  if (url === '/date') {
+    res.end(
+      JSON.stringify({
+        date: moment().format('MMMM Do YYYY'),
+      })
+    );
+  }
+  if (url === '/users') {
+    res.end(JSON.stringify(data));
+  }
+});
 
 server.listen(3000, () => {
   console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
