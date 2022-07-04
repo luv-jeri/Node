9863bb3d183526989fe8adc4646ce8cdf77c16f9
luv-jeri <luvvjeri@gmail.com>
2022-07-04 22:08:20 +0530

    -

--- database/Schemas/Todo.schema.js
+++ database/Schemas/Todo.schema.js
@@ -4,14 +4,23 @@ const todoSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    minLength: 3,
+    maxLength: 255,
+    unique: true,
   },
   description: {
     type: String,
     required: true,
+    minLength: 3,
+    maxLength: 255,
   },
   completed: {
     type: Boolean,
     default: false,
+  },
+  day: {
+    type: String,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
   },
 });
 
